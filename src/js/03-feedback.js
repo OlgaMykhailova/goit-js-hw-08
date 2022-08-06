caa8e77ae44e5799ee024aee907bf7fc893752c4
OlgaMykhailova <rosebud_z@yahoo.com>
2022-08-06 19:17:50 +0300

    updated task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,9 @@ import throttle from 'lodash.throttle';
 const inputEl = document.querySelector('input');
 const textareaEl = document.querySelector('textarea');
 const formEl = document.querySelector('.feedback-form');
-const formDataInput = {};
+const STORAGE_KEY = 'feedback-form-state';
+const formDataInput =
+  JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 fillingInput();
 
@@ -11,8 +13,11 @@ formEl.addEventListener('input', throttle(onInput, 500));
 formEl.addEventListener('submit', onSubmitClick);
 
 function onInput(event) {
-  formDataInput[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formDataInput));
+  console.log(event.target.value);
+  if (event.target.value) {
+    formDataInput[event.target.name] = event.target.value;
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataInput));
 }
 
 function onSubmitClick(event) {
@@ -21,18 +26,19 @@ function onSubmitClick(event) {
     alert('Необхідно заповнити всі поля форми');
   } else {
     event.currentTarget.reset();
-    console.log(localStorage.getItem('feedback-form-state'));
-    localStorage.removeItem('feedback-form-state');
+    console.log(localStorage.getItem(STORAGE_KEY));
+    localStorage.removeItem(STORAGE_KEY);
   }
 }
 
 function fillingInput() {
-  if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
-    const savedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (JSON.parse(localStorage.getItem(STORAGE_KEY))) {
+    const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedInput.email) {
       inputEl.value = savedInput.email;
     }
+
     if (savedInput.message) {
       textareaEl.value = savedInput.message;
     }
